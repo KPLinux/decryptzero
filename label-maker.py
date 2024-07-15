0bd21a576267595460b3f6aa6d20571cf9302f4d
KPLinux <crypticgeodash1@gmail.com>
2024-07-15 11:33:47 -0700

    full revamp to manage 40k samples

--- label-maker.py
+++ label-maker.py
@@ -1,6 +1,40 @@
 from algs import create_file
+import os
 
-i = 1
-for line in open("/home/kanishk/projects/ocr/samples/move-samples/labels.txt", 'r'):
-    create_file("/home/kanishk/projects/ocr/samples/label-samples/" + str(i) + ".gt.txt", line)
-    i = i + 1
+i = 0
+
+test_string = "hello there I am Mr. Green!"
+
+start = 0
+# for i in range(len(test_string)):
+    # if test_string[i] == ' ':
+        # print(test_string[start:i])
+        # start = i+1
+# print(test_string[start:])
+
+
+line_list = []
+for line in open("/home/kplinux/projects/ocr/samples/handwriting-generation/words_new.txt", 'r'):
+    for char in range(len(line)):
+        if line[char] == ' ':
+            line_list.append(line[start:char])
+            start = char + 1
+    line_list.append(line[start:])
+    for n in line_list:
+        if n == '':
+            line_list.remove(n)
+        line_list = list(map(lambda x: x.replace('\n',''), line_list))
+    create_file("/home/kplinux/projects/ocr/tesstrain/data/decrypt-ground-truth/" + line_list[0] + ".gt.txt", line_list[8])
+    print(line_list)
+    line_list = []
+    start = 0
+
+
+
+# print(line_list)
+
+    # create_file("/home/kplinux/projects/ocr/samples/label-samples/" + str(i) + ".gt.txt", line)
+    # i = i + 1
+    # if i == 1:
+        # break
+
