c497795452719e3c1a15e9c6400517e76a18bd6b
KPLinux <crypticgeodash1@gmail.com>
2024-07-20 20:18:36 -0700

    removed unnecessary files

--- label-maker.py
+++ label-maker.py
@@ -1,41 +1,14 @@
 from algs import create_file
+import os
+import re
+
+def extract_index(filename):
+    match = re.search(r'\d+', filename)  # Extract number from filename
+    return int(match.group()) if match else float('inf')  # Return number or infinity if not found
 
 i = 0
-
-test_string = "hello there I am Mr. Green!"
-
-start = 0
-# for i in range(len(test_string)):
-    # if test_string[i] == ' ':
-        # print(test_string[start:i])
-        # start = i+1
-# print(test_string[start:])
-
-
-line_list = []
-for line in open("/home/kplinux/projects/ocr/samples/labels1.txt", 'r'):
-    for char in range(len(line)):
-        if line[char] == ' ':
-            line_list.append(line[start:char])
-            start = char + 1
-    line_list.append(line[start:])
-    for n in line_list:
-        if n == '':
-            line_list.remove(n)
-        line_list = list(map(lambda x: x.replace('\n',''), line_list))
-    create_file("/home/kplinux/projects/ocr/samples/decrypt-ground-truth2/" 
-                + str(i) + "__" + line_list[0] + ".gt.txt", line_list[0])
-    print(line_list)
-    line_list = []
-    start = 0
+for file in sorted(os.listdir('/home/kplinux/projects/ocr/deep-text-recognition-benchmark/valid-data/samples'),
+                   key = extract_index):
+    with open('/home/kplinux/projects/ocr/deep-text-recognition-benchmark/valid-data/gt.txt', 'a') as f:
+        f.write('samples/' + file + '\t' + file.replace(str(i) + '__', '').replace('.png', '') + '\n')
     i += 1
-
-
-
-# print(line_list)
-
-    # create_file("/home/kplinux/projects/ocr/samples/label-samples/" + str(i) + ".gt.txt", line)
-    # i = i + 1
-    # if i == 1:
-        # break
-
