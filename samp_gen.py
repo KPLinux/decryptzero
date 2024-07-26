48be91367c87525f4a4bff83c99dd3e15425c754
KPLinux <crypticgeodash1@gmail.com>
2024-07-25 21:31:45 -0700

    error fix

--- samp_gen.py
+++ samp_gen.py
@@ -4,11 +4,13 @@ import glob
 import cv2
 import numpy as np
 
-pieces_list=['B', 'N', 'R', 'Q', 'K']
-files_list=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+pieces_list=['B', 'K', 'N', 'Q', 'R']
+files_list = ['a' 'b', 'c', 'd', 'e', 'f', 'g', 'h']
 ranks_list=['1', '2', '3', '4', '5', '6', '7', '8']
 endings=['+', '#']
+promotion=['=']
 captures=['x']
+cast=['O', '-', 'O']
 
 def main():
     global transformations
@@ -16,11 +18,11 @@ def main():
     samples_dir="samples"
 
     #whether you want to have transformations or not
-    transformations=True
+    transformations=False
 
     if not os.path.exists(samples_dir):
         os.makedirs(samples_dir)
-    for i in range(50000):
+    for i in range(10):
         cending=random.randint(0,8)
         if cending==1:
             cending=True
@@ -31,7 +33,17 @@ def main():
             ccapt=True
         else:
             ccapt=False
-        gen_img(directory, samples_dir, cending, ccapt, i)
+        ccast=random.randint(0,10)
+        if ccast==1:
+            ccast=True
+        else:
+            ccast=False
+        cpromo=random.randint(0,10)
+        if cpromo==1:
+            cpromo=True
+        else:
+            cpromo=False
+        gen_img(directory, samples_dir, cending, ccapt, ccast, cpromo, i)
 
 
 def apply_random_transformations(image):
@@ -83,27 +95,36 @@ def combine_imgs(images):
         padded_images=[pad_image(img, max_height) for img in images]
         return cv2.hconcat(padded_images)
 
-def gen_img(directory, samples_dir, cending, ccapt, suffix):
+def gen_img(directory, samples_dir, cending, ccapt, ccast, cpromo, suffix):
     piece=random.choice(pieces_list)
     file=random.choice(files_list)
     rank=random.choice(ranks_list)
     shach=random.choice(endings)
     capture=random.choice(captures)
+    promo=random.choice(promotion)
 
     piece_img_path=get_rand_img(directory, piece)
     file_img_path=get_rand_img(directory, file)
     rank_img_path=get_rand_img(directory, rank)
     shach_img_path=get_rand_img(directory, shach)
     capture_img_path=get_rand_img(directory, capture)
-
+    cast_img_path=[get_rand_img(directory, cast[i]) for i in range(len(cast))]
+    promo_img_path=get_rand_img(directory, promo)
 
     piece_img=cv2.imread(piece_img_path)
     file_img=cv2.imread(file_img_path)
     rank_img=cv2.imread(rank_img_path)
     shach_img=cv2.imread(shach_img_path)
     capture_img=cv2.imread(capture_img_path)
+    promo_img=cv2.imread(promo_img_path)
 
-    if not cending and not ccapt:
+    if ccast:
+        combined_img=combine_imgs([cv2.imread(cast_img_path[i]) for i in range(len(cast))])
+        base_name=f"{cast[0]}{cast[1]}{cast[2]}"
+    elif cpromo:
+        combined_img=combine_imgs([file_img, rank_img, promo_img, piece_img])
+        base_name=f"{file}{rank}{promo}{piece}"
+    elif not cending and not ccapt:
         combined_img=combine_imgs([piece_img, file_img, rank_img])
         base_name=f"{piece}{file}{rank}"
     elif cending and not ccapt:
