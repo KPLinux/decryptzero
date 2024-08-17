f605315683f8670040d29f6910dbd2eab125cd58
KPLinux <crypticgeodash1@gmail.com>
2024-08-17 15:25:34 -0700

    removed duplicate files

--- kraken-test.py
+++
@@ -1,8 +0,0 @@
-import kraken
-from kraken import pageseg, rpred
-with open('/home/kplinux/projects/ocr/samples/sheet-samples/uscf-commem-pencil.jpg', 'rb') as img:
-    image = pageseg.segment(img.read())
-
-model = rpred.load_any('/home/kplinux/.config/kraken/en_best.mlmodel')
-text = rpred.recognize(image, model)
-print(text)
